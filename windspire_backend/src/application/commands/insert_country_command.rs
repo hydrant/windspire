1d4743549c77b18b675ae6bcd34c6d67b351c3ce
Ove Stoerholt <ove@inforte.no>
2025-04-23 21:06:13 +0200

    feat: Added simple validation of CountryCreate struct

--- windspire_backend/src/application/commands/insert_country_command.rs
+++ windspire_backend/src/application/commands/insert_country_command.rs
@@ -5,6 +5,7 @@ use axum::{
 };
 use serde_json::json;
 use sqlx::PgPool;
+use validator::Validate;
 
 use crate::{
     application::common::http_reponse::json_response,
@@ -14,10 +15,20 @@ use crate::{
 
 pub async fn insert_country_command(
     State(pg_pool): State<PgPool>,
-    Json(country): Json<CountryCreate>,
+    Json(country_create): Json<CountryCreate>,
 ) -> impl IntoResponse {
+    // Validate the country_create data
+    match country_create.validate() {
+        Ok(_) => (),
+        Err(e) => {
+            return json_response(
+                StatusCode::BAD_REQUEST,
+                json!({ "success": false, "message": e }),
+            );
+        }
+    };
     let repository = SqlxCountryRepository;
-    match repository.insert_country(&pg_pool, country).await {
+    match repository.insert_country(&pg_pool, country_create).await {
         Ok(country) => json_response(StatusCode::OK, json!({ "success": true, "data": country })),
         Err(e) => json_response(
             StatusCode::INTERNAL_SERVER_ERROR,
--- windspire_backend/src/domain/models/country.rs
+++ windspire_backend/src/domain/models/country.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use validator::Validate;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Country {
@@ -9,10 +10,12 @@ pub struct Country {
     pub iso_alpha_3: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Deserialize, Serialize, Clone, Validate)]
 pub struct CountryCreate {
     pub iso_name: String,
+    #[validate(length(equal = 2, message = "ISO Alpha-2 code must be 2 characters"))]
     pub iso_alpha_2: String,
+    #[validate(length(equal = 3, message = "ISO Alpha-3 code must be 3 characters"))]
     pub iso_alpha_3: String,
 }
 
