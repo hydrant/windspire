98526e26d9c66e96a2e922dd00a0f819d8b81d0c
Ove Stoerholt <ove@inforte.no>
2025-04-25 07:45:58 +0200

    refactor: Added validation of country updates

--- windspire_backend/src/domain/models/country.rs
+++ windspire_backend/src/domain/models/country.rs
@@ -19,9 +19,11 @@ pub struct CountryCreate {
     pub iso_alpha_3: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Deserialize, Serialize, Clone, Validate)]
 pub struct CountryUpdate {
     pub iso_name: String,
+    #[validate(length(equal = 2, message = "ISO Alpha-2 code must be 2 characters"))]
     pub iso_alpha_2: String,
+    #[validate(length(equal = 3, message = "ISO Alpha-3 code must be 3 characters"))]
     pub iso_alpha_3: String,
 }
