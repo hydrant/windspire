e6bc5aa975d58d370188a7c615a0ef0b7520e207
Ove Stoerholt <ove@inforte.no>
2025-04-25 08:25:04 +0200

    refactor: Added validation of UserCreate object

--- windspire_backend/src/application/commands/insert_user_command.rs
+++ windspire_backend/src/application/commands/insert_user_command.rs
@@ -1,16 +1,38 @@
-use axum::{extract::{Json, State}, http::StatusCode, response::IntoResponse};
+use crate::{
+    application::common::http_reponse::json_response,
+    domain::{interface::user_repository::UserRepository, models::user::UserCreate},
+    infrastructure::repositories::sqlx_user_repository::SqlxUserRepository,
+};
+use axum::{
+    extract::{Json, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
 use serde_json::json;
 use sqlx::PgPool;
-use crate::{application::common::http_reponse::json_response, domain::{interface::user_repository::UserRepository, models::user::UserCreate}, infrastructure::repositories::sqlx_user_repository::SqlxUserRepository};
-
+use validator::Validate;
 
 pub async fn insert_user_command(
     State(pg_pool): State<PgPool>,
-    Json(user): Json<UserCreate>,
+    Json(user_create): Json<UserCreate>,
 ) -> impl IntoResponse {
+    // Validate the user_create data
+    match user_create.validate() {
+        Ok(_) => (),
+        Err(e) => {
+            return json_response(
+                StatusCode::BAD_REQUEST,
+                json!({ "success": false, "message": e }),
+            );
+        }
+    };
+
     let repository = SqlxUserRepository;
-    match repository.insert_user(&pg_pool, user).await {
+    match repository.insert_user(&pg_pool, user_create).await {
         Ok(users) => json_response(StatusCode::OK, json!({ "success": true, "data": users })),
-        Err(e) => json_response(StatusCode::INTERNAL_SERVER_ERROR, json!({ "success": false, "message": e.to_string() })),
+        Err(e) => json_response(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            json!({ "success": false, "message": e.to_string() }),
+        ),
     }
 }
