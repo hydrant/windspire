d2324862e0bf9054d753f316d8ffd292a5fd17e7
Ove Stoerholt <ove@inforte.no>
2025-04-25 08:25:04 +0200

    refactor: Added validation of UserCreate object

--- windspire_backend/src/domain/models/user.rs
+++ windspire_backend/src/domain/models/user.rs
@@ -1,5 +1,13 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use validator::Validate;
+
+use once_cell::sync::Lazy;
+
+static REGEX_EMAIL: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"^(?:[a-zA-Z0-9_'^&+/=?`{|}~.-]+)@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$").unwrap()
+});
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct User {
@@ -11,11 +19,16 @@ pub struct User {
     pub country_id: Uuid,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Deserialize, Serialize, Clone, Validate)]
 pub struct UserCreate {
+    #[validate(length(min = 2, message = "First name must contain 2 at least characters"))]
     pub first_name: String,
+    #[validate(length(min = 1, message = "First name must contain 1 at least characters"))]
     pub last_name: String,
+    // #[validate(email)] - does not work
+    #[validate(regex(path = *REGEX_EMAIL, message = "Email has incorrect format"))]
     pub email: String,
+    #[validate(length(min = 3, message = "Phone must contain 3 at least characters"))]
     pub phone: Option<String>,
     pub country_id: Uuid,
 }
