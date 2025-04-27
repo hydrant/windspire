413ea83b6adc0ffa604cff2ca94dfb9d09855026
Ove Stoerholt <ove@inforte.no>
2025-04-27 16:42:14 +0200

    chore: Changed JSON to use camelCase

--- windspire_backend/src/domain/models/country.rs
+++ windspire_backend/src/domain/models/country.rs
@@ -3,6 +3,7 @@ use uuid::Uuid;
 use validator::Validate;
 
 #[derive(Debug, Deserialize, Serialize, Clone, sqlx::FromRow)]
+#[serde(rename_all = "camelCase")]
 pub struct Country {
     pub id: Uuid,
     pub iso_name: String,
@@ -11,6 +12,7 @@ pub struct Country {
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, Validate)]
+#[serde(rename_all = "camelCase")]
 pub struct CountryCreate {
     pub iso_name: String,
     #[validate(length(equal = 2, message = "ISO Alpha-2 code must be 2 characters"))]
@@ -18,8 +20,8 @@ pub struct CountryCreate {
     #[validate(length(equal = 3, message = "ISO Alpha-3 code must be 3 characters"))]
     pub iso_alpha_3: String,
 }
-
 #[derive(Debug, Deserialize, Serialize, Clone, Validate)]
+#[serde(rename_all = "camelCase")]
 pub struct CountryUpdate {
     pub iso_name: String,
     #[validate(length(equal = 2, message = "ISO Alpha-2 code must be 2 characters"))]
